970219579226314b13021ea45a626cfd163e5594
DrEiZ3R <vanyax941@gmail.com>
2023-05-20 15:24:14 +0300

    fix: minor changes

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
+const dropdowns = document.querySelectorAll(".dropdown");
 const buttons = document.querySelectorAll(".dropdown__button");
 const divs = document.querySelectorAll(".dropdown__content");
+const checkbox = document.querySelector('#burger');
 
-buttons.forEach((button, i) => button.addEventListener("click", () => {
-    divs.forEach((div, j) => {
-        if (i !== j) {
-            div.style.display = "none";
-            buttons[j].classList.remove("up");
-        }
-    });
-
-    const computedStyle = window.getComputedStyle(divs[i]);
-    const displayValue = computedStyle.getPropertyValue("display");
-
-    if (displayValue === "none") {
-        divs[i].style.display = "block";
-        button.classList.add("up");
-    } else {
-        divs[i].style.display = "none";
-        button.classList.remove("up");
+const resizeListener = () => {
+    if (window.innerWidth >= 900) {
+        checkbox.checked = false;
+        divs.forEach(div => div.style.display = 'none');
     }
-}));
+}
+
+window.addEventListener('resize', resizeListener);
+
+checkbox.addEventListener('change', () => {
+    if (!this.checked) {
+        divs.forEach(div => div.style.display = 'none');
+        buttons.forEach(button => button.classList.remove("up"));
+    }
+});
+
+if (window.innerWidth <= 900) {
+    buttons.forEach((button, i) => button.addEventListener("click", () => {
+        divs.forEach((div, j) => {
+            if (i !== j) {
+                div.style.display = "none";
+                buttons[j].classList.remove("up");
+            }
+        });
+
+        const computedStyle = window.getComputedStyle(divs[i]);
+        const displayValue = computedStyle.getPropertyValue("display");
+
+        if (displayValue === "none") {
+            divs[i].style.display = "block";
+            button.classList.add("up");
+        } else {
+            divs[i].style.display = "none";
+            button.classList.remove("up");
+        }
+    }));
+}
 
 window.addEventListener('scroll', () => {
     const navbar = document.getElementById('navbar');
