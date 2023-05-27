8daeb6b95e86d5a69cf4675c2cd209aabb195a91
DrEiZ3R <vanyax941@gmail.com>
2023-05-27 18:03:57 +0300

    feat: dropdowns from objects

--- script.js
+++ script.js
@@ -1,3 +1,198 @@
+const projects = [
+    {
+        id: 1,
+        imgURL: "img/spring-boot.svg",
+        title: "Spring Boot",
+        text: "Takes an opinionated view of building Spring applications and gets you up and running as quickly as possible."
+    },
+    {
+        id: 2,
+        imgURL: "img/spring-framework.svg",
+        title: "Spring Framework",
+        text: "Provides core support for dependency injection, transaction management, web apps, data access, messaging, and more."
+    },
+    {
+        id: 3,
+        imgURL: "img/spring-data.svg",
+        title: "Spring Data",
+        text: "Provides a consistent approach to data access – relational, non-relational, map-reduce, and beyond."
+    },
+    {
+        id: 4,
+        imgURL: "img/spring-cloud.svg",
+        title: "Spring Cloud",
+        text: "Provides a set of tools for common patterns in distributed systems. Useful for building and deploying microservices."
+    },
+    {
+        id: 5,
+        imgURL: "img/spring-data-flow.svg",
+        title: "Spring Cloud Data Flow",
+        text: "Provides an orchestration service for composable data microservice applications on modern runtimes."
+    },
+    {
+        id: 6,
+        imgURL: "img/spring-security.svg",
+        title: "Spring Security",
+        text: "Protects your application with comprehensive and extensible authentication and authorization support."
+    }
+]
+
+const dropdowns = [
+    {
+        id: 1,
+        link: "Why Spring",
+        links: [
+            "Overview",
+            "Microservices",
+            "Reactive",
+            "Event Driven",
+            "Cloud",
+            "Web Applications",
+            "Serverless",
+            "Batch"
+        ]
+    },
+    {
+        id: 2,
+        link: "Learn",
+        links: [
+            "Overview",
+            "Quickstart",
+            "Guides",
+            "Blog"
+        ]
+    },
+    {
+        id: 3,
+        link: "Projects",
+        links: [
+            "Overview",
+            "Spring Boot",
+            "Spring Framework",
+            "Spring Cloud",
+            "Spring Cloud Data Flow",
+            "Spring Data",
+            "Spring Integration",
+            "Spring Batch",
+            "Spring Security",
+            "View all projects",
+            "Development tools",
+            "Spring Tools4",
+            "Spring Initializr"
+        ]
+    },
+    {
+        id: 4,
+        link: "Academy",
+        links: [
+            "Courses",
+            "Get Certified"
+        ]
+    },
+    {
+        id: 5,
+        link: "Support",
+        links: [
+            "Overview",
+            "Security Advisories"
+        ]
+    },
+    {
+        id: 6,
+        link: "Community",
+        links: [
+            "Overview",
+            "Events",
+            "Team"
+        ]
+    }
+]
+
+function loadDropdowns() {
+    const list = document.querySelector(".nav__list");
+
+    for (let i = 0; i < dropdowns.length; i++) {
+        const dropdown = dropdowns[i];
+
+        const wrapper = document.createElement("div");
+        wrapper.classList.add("dropdown");
+
+        const button = document.createElement("button");
+        button.classList.add("dropdown__button");
+        button.textContent = dropdown.link;
+
+        const linksDiv = document.createElement("div");
+        linksDiv.classList.add("dropdown__content");
+
+        const links = dropdown.links.map((l, index) => {
+            //for blue link and span in dropdown "Projects"
+            if ((dropdown.id === 3 && index === 9) || (dropdown.id === 3 && index === 10)) {
+                if (index === 9) {
+                    const blueLink = document.createElement("a");
+                    blueLink.href = "#";
+                    blueLink.id = "blue";
+                    blueLink.textContent = l;
+                    return blueLink;
+                } else {
+                    const span = document.createElement("span");
+                    span.classList.add("projects__text__tools");
+                    span.textContent = l;
+                    return span;
+                }
+            } else { //all other links
+                const link = document.createElement("a");
+                link.href = "#";
+                link.textContent = l;
+                return link;
+            }
+        });
+
+        links.forEach(l => linksDiv.appendChild(l));
+        wrapper.appendChild(button);
+        wrapper.appendChild(linksDiv);
+
+        list.appendChild(wrapper);
+    }
+}
+
+function loadProjects() {
+    const wrapper = document.querySelector(".projects__table");
+
+    for (let i = 0; i < projects.length; i++) {
+        const project = projects[i];
+
+        const article = document.createElement("article");
+
+        const link = document.createElement("a");
+        link.href = "#";
+
+        const image = document.createElement("img");
+        image.src = project.imgURL;
+        image.alt = "";
+        image.classList.add("project__icon");
+
+        const textDiv = document.createElement("div");
+        textDiv.classList.add("project__text");
+
+        const title = document.createElement("h2");
+        title.textContent = project.title;
+
+        const text = document.createElement("p");
+        text.textContent = project.text;
+
+        textDiv.appendChild(title);
+        textDiv.appendChild(text);
+        link.appendChild(image);
+        link.appendChild(textDiv);
+        article.appendChild(link);
+
+        wrapper.appendChild(article);
+    }
+}
+
+loadDropdowns();
+loadProjects();
+
 const buttons = document.querySelectorAll(".dropdown__button");
 const divs = document.querySelectorAll(".dropdown__content");
 const checkbox = document.querySelector('#burger');
@@ -53,81 +248,3 @@ window.addEventListener('scroll', () => {
         scrollbar.classList.remove('show-scrollbar');
     }
 });
-
-// loaders
-
-const projects = [
-    {
-        id: 1,
-        imgURL: "img/spring-boot.svg",
-        title: "Spring Boot",
-        text: "Takes an opinionated view of building Spring applications and gets you up and running as quickly as possible."
-    },
-    {
-        id: 2,
-        imgURL: "img/spring-framework.svg",
-        title: "Spring Framework",
-        text: "Provides core support for dependency injection, transaction management, web apps, data access, messaging, and more."
-    },
-    {
-        id: 3,
-        imgURL: "img/spring-data.svg",
-        title: "Spring Data",
-        text: "Provides a consistent approach to data access – relational, non-relational, map-reduce, and beyond."
-    },
-    {
-        id: 4,
-        imgURL: "img/spring-cloud.svg",
-        title: "Spring Cloud",
-        text: "Provides a set of tools for common patterns in distributed systems. Useful for building and deploying microservices."
-    },
-    {
-        id: 5,
-        imgURL: "img/spring-data-flow.svg",
-        title: "Spring Cloud Data Flow",
-        text: "Provides an orchestration service for composable data microservice applications on modern runtimes."
-    },
-    {
-        id: 6,
-        imgURL: "img/spring-security.svg",
-        title: "Spring Security",
-        text: "Protects your application with comprehensive and extensible authentication and authorization support."
-    },
-]
-
-const loadProjects = () => {
-    const wrapper = document.querySelector(".projects__table");
-
-    for (let i = 0; i < projects.length; i++) {
-        const project = projects[i];
-
-        const article = document.createElement("article");
-
-        const link = document.createElement("a");
-        link.href = "#";
-
-        const image = document.createElement("img");
-        image.src = project.imgURL;
-        image.alt = "";
-        image.classList.add("project__icon");
-
-        const textDiv = document.createElement("div");
-        textDiv.classList.add("project__text");
-
-        const title = document.createElement("h2");
-        title.textContent = project.title;
-
-        const text = document.createElement("p");
-        text.textContent = project.text;
-
-        textDiv.appendChild(title);
-        textDiv.appendChild(text);
-        link.appendChild(image);
-        link.appendChild(textDiv);
-        article.appendChild(link);
-
-        wrapper.appendChild(article);
-    }
-}
-
-loadProjects();
